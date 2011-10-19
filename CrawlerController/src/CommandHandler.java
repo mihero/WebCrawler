be702acfef323c43085b57abe87c6d00fb2a0a5e
Mikko Rosten <mikko.rosten@iki.fi>
2011-10-19 19:46:41 +0300

    commandhandler intial version

--- CrawlerController/src/CommandHandler.java
+++ CrawlerController/src/CommandHandler.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -5,13 +9,103 @@ import java.net.Socket;
  * @author Teemu Miettinen tpjmie@utu.fi
  */
 public class CommandHandler extends Thread {
+	
+	private enum Commands {
+		LIST,
+		KILL,
+		COUNT,
+		SITES
+		
+	}
+	private Socket client;
+	private SearchController SC;
+	private PrintWriter out;
+	private BufferedReader in;
 
 	public CommandHandler(SearchController controller, Socket soc){
-		
+		client=soc;
+		SC=controller;
 	}
 	public void run(){
 		/**
 		 * listen for socket and handle commands received from there
 		 */
+		try {
+			out = new PrintWriter(client.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		
+			String cmd;
+	        while((cmd=in.readLine()) != null){
+	        	handleCommand(cmd);
+	        }
+        
+        
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	private void handleCommand(String cmd){
+		
+		String[] args = cmd.split(" ");
+		Commands command = Commands.valueOf(args[0].toUpperCase());
+		
+		switch (command){
+		case LIST:
+			doList();
+			
+			break;
+		case KILL:
+			if (args.length<2){
+				doInvalid();
+			}
+			else{
+				try{
+					int id=Integer.parseInt(args[1]);
+					doKill(id);
+				}
+				catch(NumberFormatException e){
+					System.err.println(e.toString());
+					doInvalid();
+				}
+				
+			}
+						
+			break;
+		case COUNT:
+			doCount();
+			break;
+		case SITES:
+			doSiteCount();
+			break;
+		default:
+			doInvalid();
+			break;
+		}
+		
+	}
+	private void doList(){
+		String[] crawlers=SC.getCrawlerList();
+		/**
+		 * print to output list of crawlers
+		 */
+	}
+	private void doKill(int id){
+		SC.killCrawler(id);
+		
+		
+	}
+	private void doCount(){
+		SC.getNumberOfCrawlers();
+		
+	}
+	private void doSiteCount(){
+		SC.getFoundHits();
+	}
+	private void doInvalid(){
+		
 	}
 }
